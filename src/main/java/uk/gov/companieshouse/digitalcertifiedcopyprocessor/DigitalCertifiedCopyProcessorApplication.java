cdae324ef9fce3dc63a8b82efa0080053d956195
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2023-02-07 15:14:17 +0000

    Initial skeleton app commit

---
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/DigitalCertifiedCopyProcessorApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.digitalcertifiedcopyprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCertifiedCopyProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalCertifiedCopyProcessorApplication.class, args);
+    }
+
+}
