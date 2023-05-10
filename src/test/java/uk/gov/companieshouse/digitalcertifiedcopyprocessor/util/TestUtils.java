7dca23dd43512cc7ff86ebe8a656fe421eb42e5b
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-05-10 13:55:09 +0100

    DCAC-70: Add a manual test for get document metadata REST call.

--- src/test/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/util/TestUtils.java
+++ src/test/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/util/TestUtils.java
@@ -3,6 +3,8 @@ package uk.gov.companieshouse.digitalcertifiedcopyprocessor.util;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.springframework.core.env.Environment;
 
+import java.util.Map;
+
 public class TestUtils {
 
     /**
@@ -18,6 +20,32 @@ public class TestUtils {
         variables.set("API_URL", "http://localhost:" + wireMockPort);
         variables.set("PAYMENTS_API_URL", "http://localhost:" + wireMockPort);
         return wireMockPort;
+    }
+
+    /**
+     * Configures the CH Java SDKs used in manual tests to interact with Tilt
+     * hosted API endpoints.
+     * @param variables {@link EnvironmentVariables} class rule permitting environment variable manipulation
+     * @see #givenSdkIsConfigured(EnvironmentVariables, Map)
+     */
+    public static void givenSdkIsConfiguredForTilt(final EnvironmentVariables variables) {
+        variables.set("CHS_API_KEY", "MGQ1MGNlYmFkYzkxZTM2MzlkNGVmMzg4ZjgxMmEz");
+        variables.set("API_URL", "http://api.chs.local:4001");
+        variables.set("PAYMENTS_API_URL", "http://api.chs.local:4001");
+    }
+
+    /**
+     * Configures the CH Java SDKs used in manual tests to interact with API endpoints.
+     * @param variables {@link EnvironmentVariables} class rule permitting environment variable manipulation
+     * @param variableValues map of key-value pairs to override settings which otherwise default to acceptable
+     *                       values for testing with Tilt hosted API endpoints
+     */
+    public static void givenSdkIsConfigured(final EnvironmentVariables variables,
+                                            final Map<String, String> variableValues) {
+        variables.set("CHS_API_KEY", "MGQ1MGNlYmFkYzkxZTM2MzlkNGVmMzg4ZjgxMmEz");
+        variables.set("API_URL", "http://api.chs.local:4001");
+        variables.set("PAYMENTS_API_URL", "http://api.chs.local:4001");
+        variableValues.forEach(variables::set);
     }
 
 }
