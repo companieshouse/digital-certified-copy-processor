28d6ec5fc9f8ce5a5815fa882372cecaab6aa3f1
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-05-11 12:42:13 +0100

    DCAC-70: SonarLint sweep.

--- src/test/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/DigitalCertifiedCopyProcessorApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/DigitalCertifiedCopyProcessorApplicationTests.java
@@ -1,12 +1,15 @@
 package uk.gov.companieshouse.digitalcertifiedcopyprocessor;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DigitalCertifiedCopyProcessorApplicationTests {
 
-    @Test
+	@SuppressWarnings("squid:S2699") // at least one assertion
+	@DisplayName("context loads")
+	@Test
 	void contextLoads() {
 	}
 
