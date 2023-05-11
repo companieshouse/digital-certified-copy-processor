a6f9a01b304e1862e7219e1fd4bfde0518e63039
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-05-11 12:13:32 +0100

    DCAC-70: Enforce presence of required env vars on start up.

--- src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/DigitalCertifiedCopyProcessorApplication.java
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/DigitalCertifiedCopyProcessorApplication.java
@@ -3,13 +3,17 @@ package uk.gov.companieshouse.digitalcertifiedcopyprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static uk.gov.companieshouse.digitalcertifiedcopyprocessor.environment.EnvironmentVariablesChecker.allRequiredEnvironmentVariablesPresent;
+
 @SpringBootApplication
 public class DigitalCertifiedCopyProcessorApplication {
 
     public static final String NAMESPACE = "digital-certified-copy-processor";
 
     public static void main(String[] args) {
-        SpringApplication.run(DigitalCertifiedCopyProcessorApplication.class, args);
+        if (allRequiredEnvironmentVariablesPresent()) {
+            SpringApplication.run(DigitalCertifiedCopyProcessorApplication.class, args);
+        }
     }
 
 }
---
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/environment/EnvironmentVariablesChecker.java
@@ -0,0 +1,47 @@
+package uk.gov.companieshouse.digitalcertifiedcopyprocessor.environment;
+
+import uk.gov.companieshouse.environment.EnvironmentReader;
+import uk.gov.companieshouse.environment.exception.EnvironmentVariableException;
+import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import static uk.gov.companieshouse.digitalcertifiedcopyprocessor.DigitalCertifiedCopyProcessorApplication.NAMESPACE;
+
+public class EnvironmentVariablesChecker {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
+
+    public enum RequiredEnvironmentVariables {
+        API_URL("API_URL"),
+        PAYMENTS_API_URL("PAYMENTS_API_URL"),
+        CHS_API_KEY("CHS_API_KEY");
+
+        private final String name;
+
+        RequiredEnvironmentVariables(String name) { this.name = name; }
+
+        public String getName() { return this.name; }
+    }
+
+    /**
+     * Method to check if all of the required configuration variables
+     * defined in the RequiredEnvironmentVariables enum have been set to a value
+     * @return <code>true</code> if all required environment variables have been set, <code>false</code> otherwise
+     */
+    public static boolean allRequiredEnvironmentVariablesPresent() {
+        EnvironmentReader environmentReader = new EnvironmentReaderImpl();
+        var allVariablesPresent = true;
+        LOGGER.info("Checking all environment variables present");
+        for(RequiredEnvironmentVariables param : RequiredEnvironmentVariables.values()) {
+            try{
+                environmentReader.getMandatoryString(param.getName());
+            } catch (EnvironmentVariableException eve) {
+                allVariablesPresent = false;
+                LOGGER.error(String.format("Required config item %s missing", param.getName()));
+            }
+        }
+
+        return allVariablesPresent;
+    }
+}
