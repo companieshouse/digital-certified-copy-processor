3cff7e0eb95fb4aadabdcc3459ec31880695d1c7
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-05-11 08:54:34 +0100

    DCAC-70: Use structured logging.

--- src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/FilingHistoryDocumentService.java
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/FilingHistoryDocumentService.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.digitalcertifiedcopyprocessor.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriTemplate;
@@ -8,8 +9,9 @@ import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.filinghistory.FilingApi;
 import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.util.DataMap;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -41,22 +43,22 @@ public class FilingHistoryDocumentService {
     public String getDocumentMetadata(
             final String companyNumber,
             final String filingHistoryDocumentId) {
-
-        final Map<String, Object> logMap = new HashMap<>();// TODO DCAC-70 createLogMapWithCompanyNumber(companyNumber);
         logger.info("Getting filing history document " + filingHistoryDocumentId + " for company number "
-                + companyNumber + ".", logMap);
+                + companyNumber + ".", getLogMap(companyNumber, filingHistoryDocumentId));
         final ApiClient apiClient = apiClientService.getInternalApiClient();
         final String uri = GET_FILING_HISTORY_DOCUMENT.expand(companyNumber, filingHistoryDocumentId).toString();
         try {
             final FilingApi filing = apiClient.filing().get(uri).execute().getData();
-            return filing.getLinks().getDocumentMetaData();
+            final String metadata = filing.getLinks().getDocumentMetaData();
+            logger.info("Got document metadata " + metadata + ".",
+                    getLogMap(companyNumber, filingHistoryDocumentId, metadata));
+            return metadata;
         } catch (ApiErrorResponseException ex) {
             throw getResponseStatusException(ex, apiClient, companyNumber, filingHistoryDocumentId, uri);
         } catch (URIValidationException ex) {
             // Should this happen (unlikely), it is a broken contract, hence 500.
             final String error = "Invalid URI " + uri + " for filing";
-            // TODO DCAC-70 logErrorWithStatus(logMap, error, INTERNAL_SERVER_ERROR);
-            logger.error(error, ex, logMap);
+            logger.error(error, ex, getLogMap(companyNumber, filingHistoryDocumentId, INTERNAL_SERVER_ERROR, error));
             throw new ResponseStatusException(INTERNAL_SERVER_ERROR, error);
         }
 
@@ -76,23 +78,52 @@ public class FilingHistoryDocumentService {
                                                                final String companyNumber,
                                                                final String filingHistoryDocumentId,
                                                                final String uri) {
-
-        final Map<String, Object> logMap = new HashMap<>();// TODO createLogMapWithCompanyNumber(companyNumber);
         final ResponseStatusException propagatedException;
         if (apiException.getStatusCode() >= INTERNAL_SERVER_ERROR.value()) {
             final String error = "Error sending request to "
                     + client.getBasePath() + uri + ": " + apiException.getStatusMessage();
-            // TODO DCAC-70 logErrorWithStatus(logMap, error, INTERNAL_SERVER_ERROR);
-            logger.error(error, apiException, logMap);
+            logger.error(error, apiException,
+                    getLogMap(companyNumber, filingHistoryDocumentId, INTERNAL_SERVER_ERROR, error));
             propagatedException = new ResponseStatusException(INTERNAL_SERVER_ERROR, error);
         } else {
             final String error = "Error getting filing history document " + filingHistoryDocumentId +
                     " for company number " + companyNumber + ".";
-            // TODO DCAC-70 logErrorWithStatus(logMap, error, BAD_REQUEST);
-            logger.error(error, apiException, logMap);
+            logger.error(error, apiException, getLogMap(companyNumber, filingHistoryDocumentId, BAD_REQUEST, error));
             propagatedException =  new ResponseStatusException(BAD_REQUEST, error);
         }
         return propagatedException;
+    }
+
+    private Map<String, Object> getLogMap(final String companyNumber, final String filingHistoryDocumentId) {
+        return new DataMap.Builder()
+                .companyNumber(companyNumber)
+                .filingHistoryDocumentId(filingHistoryDocumentId)
+                .build()
+                .getLogMap();
+    }
+
+    private Map<String, Object> getLogMap(final String companyNumber,
+                                          final String filingHistoryDocumentId,
+                                          final String metadata) {
+        return new DataMap.Builder()
+                .companyNumber(companyNumber)
+                .filingHistoryDocumentId(filingHistoryDocumentId)
+                .filingHistoryDocumentMetadata(metadata)
+                .build()
+                .getLogMap();
+    }
+
+    private Map<String, Object> getLogMap(final String companyNumber,
+                                          final String filingHistoryDocumentId,
+                                          final HttpStatus status,
+                                          final String error) {
+        return new DataMap.Builder()
+                .companyNumber(companyNumber)
+                .filingHistoryDocumentId(filingHistoryDocumentId)
+                .status(status.toString())
+                .errors(List.of(error))
+                .build()
+                .getLogMap();
     }
 
 }
