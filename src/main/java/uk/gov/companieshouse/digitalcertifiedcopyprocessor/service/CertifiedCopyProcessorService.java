409a4c2b49d6f1584a388327e59b03f591f285b6
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-07-18 16:13:14 +0100

    DCAC-260: Make a start on hooking up the document service.

--- src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/CertifiedCopyProcessorService.java
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/CertifiedCopyProcessorService.java
@@ -12,23 +12,32 @@ import uk.gov.companieshouse.logging.Logger;
 class CertifiedCopyProcessorService implements KafkaService {
 
     private final Logger logger;
-
     private final FilingHistoryDocumentService filingHistoryDocumentService;
+    private final DocumentService documentService;
 
-    CertifiedCopyProcessorService(Logger logger, FilingHistoryDocumentService filingHistoryDocumentService) {
+    CertifiedCopyProcessorService(Logger logger, FilingHistoryDocumentService filingHistoryDocumentService, DocumentService documentService) {
         this.logger = logger;
         this.filingHistoryDocumentService = filingHistoryDocumentService;
+        this.documentService = documentService;
     }
 
     @Override
     public void processMessage(KafkaServiceParameters parameters) {
 
         // Note MessageLoggingAspect has already done a good job of logging incoming message.
+
         final var message = parameters.getData();
         final var documentMetadata = filingHistoryDocumentService.getDocumentMetadata(
                 message.getCompanyNumber(),
                 message.getFilingHistoryId());
+
         // TODO DCAC-260 Structure or remove
-        logger.info("\nDocumentMetadata: " + documentMetadata);
+        logger.info("Document metadata: " + documentMetadata);
+
+        final var privateUri = documentService.getPrivateUri(documentMetadata);
+
+        // TODO DCAC-260 Structure or remove
+        logger.info("Private URI: " + privateUri);
+
     }
 }
