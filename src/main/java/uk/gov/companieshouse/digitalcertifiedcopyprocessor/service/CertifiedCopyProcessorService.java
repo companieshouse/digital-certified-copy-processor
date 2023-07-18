27bb0f205347bb3ad08415fc87387d4d4ab08b97
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-07-18 16:25:55 +0100

    DCAC-260: Make a start on hooking up the kafka producer service.

--- src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/CertifiedCopyProcessorService.java
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/CertifiedCopyProcessorService.java
@@ -14,11 +14,13 @@ class CertifiedCopyProcessorService implements KafkaService {
     private final Logger logger;
     private final FilingHistoryDocumentService filingHistoryDocumentService;
     private final DocumentService documentService;
+    private final KafkaProducerService kafkaProducerService;
 
-    CertifiedCopyProcessorService(Logger logger, FilingHistoryDocumentService filingHistoryDocumentService, DocumentService documentService) {
+    CertifiedCopyProcessorService(Logger logger, FilingHistoryDocumentService filingHistoryDocumentService, DocumentService documentService, KafkaProducerService kafkaProducerService) {
         this.logger = logger;
         this.filingHistoryDocumentService = filingHistoryDocumentService;
         this.documentService = documentService;
+        this.kafkaProducerService = kafkaProducerService;
     }
 
     @Override
@@ -26,18 +28,23 @@ class CertifiedCopyProcessorService implements KafkaService {
 
         // Note MessageLoggingAspect has already done a good job of logging incoming message.
 
-        final var message = parameters.getData();
+        final var certifiedCopy = parameters.getData();
         final var documentMetadata = filingHistoryDocumentService.getDocumentMetadata(
-                message.getCompanyNumber(),
-                message.getFilingHistoryId());
+                certifiedCopy.getCompanyNumber(),
+                certifiedCopy.getFilingHistoryId());
 
         // TODO DCAC-260 Structure or remove
         logger.info("Document metadata: " + documentMetadata);
+
+        // TODO DCAC-260 The code below this point has not even been manually tested
+        // in this integration context as yet.
 
         final var privateUri = documentService.getPrivateUri(documentMetadata);
 
         // TODO DCAC-260 Structure or remove
         logger.info("Private URI: " + privateUri);
+
+        kafkaProducerService.sendMessage(certifiedCopy, privateUri);
 
     }
 }
