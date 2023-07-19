d6cc0c8bee0acf66a62a8378b434949fd1b86563
tombradley1 <tbradley@companieshouse.gov.uk>
2023-07-19 17:41:55 +0100

    update logging

--- src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/FilingHistoryDescriptionService.java
+++ src/main/java/uk/gov/companieshouse/digitalcertifiedcopyprocessor/service/FilingHistoryDescriptionService.java
@@ -13,6 +13,7 @@ public class FilingHistoryDescriptionService {
 
     private final Logger logger;
     private Map<String, String> filingHistoryDescriptions;
+    private final String FILING_HISTORY_DESCRIPTION_KEY = "description";
 
     public FilingHistoryDescriptionService(
             @Value("classpath:api-enumerations/filing_history_descriptions.yml") Resource fileResource,
@@ -25,15 +26,24 @@ public class FilingHistoryDescriptionService {
         try (InputStream inputStream = fileResource.getInputStream()){
             Yaml yaml = new Yaml();
             Map<String, Object> data = yaml.load(inputStream);
-            if(data != null && data.containsKey("description") && data.get("description") instanceof Map)
-                filingHistoryDescriptions = (Map<String, String>) data.get("description");
-        } catch (IOException e) {
-            logger.error("Failed to load filing_history_descriptions from file");
+            if(data != null && data.containsKey("description") && data.get(FILING_HISTORY_DESCRIPTION_KEY) instanceof Map)
+                filingHistoryDescriptions = (Map<String, String>) data.get(FILING_HISTORY_DESCRIPTION_KEY);
+        } catch (IOException ex) {
+            String errorMessage = "Failed to load filing_history_descriptions from file," +
+                    " all descriptions will default to blank strings";
+            logger.error(errorMessage, ex);
         }
     }
 
-    public String getDescription(String name) {
+    public String getDescription(String DescriptionCode) {
         //if we can't match the description, just pass along an empty string
-        return filingHistoryDescriptions.getOrDefault(name, "");
+         String convertedDescription = filingHistoryDescriptions.get(DescriptionCode);
+         if (convertedDescription == null) {
+             String errorMessage = String.format("Description not found for FilingHistoryDescription code: " +
+                             "'%s' Defaulting to an empty string", DescriptionCode);
+             logger.error(errorMessage);
+             return "";
+         }
+        return convertedDescription;
     }
 }
